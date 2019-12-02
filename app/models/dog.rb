df16c5131c8df65073e844ed53fc6cd8e3905e38
Christian Perez <perezchristian1012@gmail.com>
2019-12-02 20:51:37 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -4,9 +4,14 @@ attr_accessor :name, :breed, :age
 
 @@all = []
 
-  def initialize
-    # @name = name
-    # @breed = breed
-    # @age = age
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 end
