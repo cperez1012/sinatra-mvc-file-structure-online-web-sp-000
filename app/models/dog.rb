ea63eb899caa1cdf3e723f73f19fe00d4b8d687d
Christian Perez <perezchristian1012@gmail.com>
2019-12-02 20:48:07 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,8 +3,8 @@ class Dog
 attr_accessor :name, :breed, :age
 
   def initialize
-    @name = name
-    @breed = breed
-    @age = age
+    # @name = name
+    # @breed = breed
+    # @age = age
   end
 end
